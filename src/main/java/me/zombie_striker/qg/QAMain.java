c8dec4dc6c94c45842e5f07c3d88bdf193cbdcf4
DavideTW <paolazzidavide67@gmail.com>
2024-12-11 11:06:40 +0100

    hardcoded server version

--- src/main/java/me/zombie_striker/qg/QAMain.java
+++ src/main/java/me/zombie_striker/qg/QAMain.java
@@ -60,7 +60,6 @@ import java.util.stream.Collectors;
 public class QAMain extends JavaPlugin {
 
     public static final int ViaVersionIdfor_1_8 = 106;
-    private static final String SERVER_VERSION;
     public static HashMap<MaterialStorage, Gun> gunRegister = new LinkedHashMap<>();
     public static HashMap<MaterialStorage, Ammo> ammoRegister = new LinkedHashMap<>();
     public static HashMap<MaterialStorage, CustomBaseObject> miscRegister = new LinkedHashMap<>();
@@ -227,13 +226,6 @@ public class QAMain extends JavaPlugin {
     public static boolean AutoDetectResourcepackVersion = true;
     private static QAMain main;
 
-    static {
-        String name = Bukkit.getServer().getClass().getName();
-        name = name.substring(name.indexOf("craftbukkit.") + "craftbukkit.".length());
-        name = name.substring(0, name.indexOf("."));
-        SERVER_VERSION = name;
-    }
-
     private TreeFellerHandler tfh = null;
     private FileConfiguration config;
     private File configFile;
@@ -244,20 +236,6 @@ public class QAMain extends JavaPlugin {
     }
 
     public static boolean isVersionHigherThan(int mainVersion, int secondVersion) {
-        String firstChar = SERVER_VERSION.substring(1, 2);
-        int fInt = Integer.parseInt(firstChar);
-        if (fInt < mainVersion)
-            return false;
-        StringBuilder secondChar = new StringBuilder();
-        for (int i = 3; i < 10; i++) {
-            if (SERVER_VERSION.charAt(i) == '_' || SERVER_VERSION.charAt(i) == '.')
-                break;
-            secondChar.append(SERVER_VERSION.charAt(i));
-        }
-
-        int sInt = Integer.parseInt(secondChar.toString());
-        if (sInt < secondVersion)
-            return false;
         return true;
     }
 
